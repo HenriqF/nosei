1d3b60fed518d472650564c9d6ece221905430fb
Henrique Reinaldi <carlosofalmeida@gmail.com>
2026-07-13 02:02:28 -0300

    Add files via upload

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ import (
 	"io"
 	"log"
 	"net"
+	"os"
 	"strings"
+)
+
+const (
+	data_name string = "data.txt"
 )
 
 func err_hand(err error, msg string) {
@@ -15,31 +20,63 @@ func err_hand(err error, msg string) {
 	}
 }
 
-func req_hand(c net.Conn) {
-	defer c.Close()
+func nc_err_hand(err error, msg string) bool {
+	if err != nil {
+		fmt.Printf("%v\n\ndeu merda:\n\n%v\n", msg, err)
+		return true
+	}
+	return false
+}
 
-	fmt.Printf("Conex: %v", c)
+func req_hand(c net.Conn, input_chain chan string) {
+	defer c.Close()
+	fmt.Printf("Conex: %v\n", c)
 
 	red := bufio.NewReader(c)
 	for {
 		msg, err := red.ReadString('\n')
+
 		if err == io.EOF {
 			return
 		}
 		err_hand(err, "reader")
 
-		fmt.Printf("receba: %v\n", msg)
-		_, err = c.Write([]byte("echo:" + msg))
-		err_hand(err, "echo")
+		fmt.Printf("%v: %v\n", c, msg)
 
-		if d := strings.TrimSpace(msg); d == "PAROOU" {
+		var echo string
+
+		d := strings.TrimSpace(msg)
+		if d == "PAROU" {
 			return
+		} else if strings.HasPrefix(d, "WRITE ") {
+			resto := d[6:]
+			input_chain <- resto
+
+			echo = fmt.Sprintf("WROTE: [%v]\n", resto)
 		}
+
+		_, err = c.Write([]byte(echo))
+		err_hand(err, "echo")
 	}
 
 }
 
+func linewriter_init(file *os.File, ic chan string, ok chan bool) {
+	for line := range ic {
+		file.WriteString(line + "\n")
+	}
+
+	ok <- true
+}
+
 func main() {
+	file, _ := os.OpenFile(data_name, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	defer file.Close()
+
+	input_chain := make(chan string, 50)
+	lw_done := make(chan bool)
+	go linewriter_init(file, input_chain, lw_done)
+
 	listener, err := net.Listen("tcp", ":6767")
 	err_hand(err, "init")
 	defer listener.Close()
@@ -47,7 +84,6 @@ func main() {
 	for {
 		c, err := listener.Accept()
 		err_hand(err, "accept")
-		go req_hand(c)
+		go req_hand(c, input_chain)
 	}
-
 }
